0469a305083096322b0d36bfb23436820a7b6dce
YJS <yjs6156@naver.com>
2022-09-03 14:28:53 +0900

    js

---
+++ P1Board/src/main/java/com/example/p1board/P1BoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.p1board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1BoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P1BoardApplication.class, args);
+    }
+
+}
---
+++ P1Board/src/test/java/com/example/p1board/P1BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.p1board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
