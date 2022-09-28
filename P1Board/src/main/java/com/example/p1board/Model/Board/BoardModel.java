a6051afdbc374e9b5e007f60eb88fefd7058c0df
YJS <yjs6156@naver.com>
2022-09-28 22:03:30 +0900

    12

--- P1Board/src/main/java/com/example/p1board/Model/Board/BoardModel.java
+++ P1Board/src/main/java/com/example/p1board/Model/Board/BoardModel.java
@@ -1,17 +1,25 @@
 package com.example.p1board.Model.Board;
 
 import com.example.p1board.Model.Member.MemberModel;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
+@EntityListeners(AuditingEntityListener.class)
 public class BoardModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,4 +32,8 @@ public class BoardModel {
     private String title;//글제목
 
     private String content;//글내용
+
+    @CreatedDate
+    @DateTimeFormat(pattern ="HH:mm")
+    private LocalTime time; //실시간 날짜
 }
--- P1Board/src/main/java/com/example/p1board/P1BoardApplication.java
+++ P1Board/src/main/java/com/example/p1board/P1BoardApplication.java
@@ -2,8 +2,11 @@ package com.example.p1board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class P1BoardApplication {
 
     public static void main(String[] args) {
